e718e8ec9424ebcfd5a6ec6f94ec6bba363c6024
XLATE <bruno-roberto@mail.ru>
2021-06-12 15:17:45 +0300

    Добавьте файлы проекта.

---
+++ REST API/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace REST_API
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Datum
+    {
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string supply { get; set; }
+        public object maxSupply { get; set; }
+        public string marketCapUsd { get; set; }
+        public string volumeUsd24Hr { get; set; }
+        public string priceUsd { get; set; }
+        public string changePercent24Hr { get; set; }
+        public string vwap24Hr { get; set; }
+        public string explorer { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Datum> data { get; set; }
+        public long timestamp { get; set; }
+    }
+}
