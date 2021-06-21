80f202cb289669035717ae5817fa30707dfd5ba9
XLATE <bruno-roberto@mail.ru>
2021-06-21 11:57:45 +0300

    update bot

--- REST API/WorkWithAPI.cs
+++ REST API/WorkWithAPI.cs
@@ -8,6 +8,7 @@ namespace REST_API
 {
     class WorkWithAPI
     {
+        [Obsolete]
         public static void Main()
         {
             TelegramBotClient bot = new TelegramBotClient("1730877816:AAEthbQ8wi87EC721d76lLyin3vtjqVNuUs");
@@ -32,8 +33,25 @@ namespace REST_API
                 {
                     string result = streamReader.ReadToEnd();
                     var coinsForecast = JsonConvert.DeserializeObject<Root>(result);
-                    Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
-                    bot.SendTextMessageAsync(arg.Message.Chat.Id, coinsForecast.data[0].priceUsd);
+
+                    try
+                    {
+                        bot.SendTextMessageAsync(arg.Message.Chat.Id,
+                            $"Name: {coinsForecast.data[0].name}\n" +
+                            $"USD price: {coinsForecast.data[0].priceUsd}$\n" +
+                            $"Position in top: {coinsForecast.data[0].rank}\n" +
+                            $"Capitalization: {coinsForecast.data[0].marketCapUsd}$\n" +
+                            $"Volume in 24 hours: {coinsForecast.data[0].volumeUsd24Hr}$\n" +
+                            $"Changes in 24 hours: {coinsForecast.data[0].changePercent24Hr}%\n");
+
+                        Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                    }
+
+                    catch (Exception)
+                    {
+                        bot.SendTextMessageAsync(arg.Message.Chat.Id, "Error: unknown cryptocurrency");
+                        Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                    }
                 }
             };
             bot.StartReceiving();
